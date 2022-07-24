f526c05991a959e9cdec3205b8a8ba1ae0873fb3
Flufyfox <hseo0729@gmail.com>
2022-07-24 13:39:13 +0900

    consol commit

--- src/Example1/Example1.java
+++ src/Example1/Example1.java
@@ -1,6 +1,7 @@
 
 public class Example{
-public static void main(String[]args){
-        System.out.println("나서스 똥챔");
+    public static void main(String[]args){
+        System.out.println("dev commit");
+
     }
 }
